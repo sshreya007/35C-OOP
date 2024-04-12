7f84efc581a3a0ecd2ee46ba8ca48e363fd9f9d8
shreya <shreyakhadka007@gmail.com>
2024-04-12 12:29:04 +0545

    Week 1 TAsk 1 Question

--- week1/tasks/Hello.java
+++ week1/tasks/Hello.java
@@ -1,5 +1,9 @@
-package tasks.35C-OOP.week1;
+package week1;
 
 public class Hello {
+    public static void main(String[] args) {
+        /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
+        System.out.println(x:"Hello Worls")
+    }
     
 }
