591fbff8999f8378a308ef553533665134bb5365
shreya <shreyakhadka007@gmail.com>
2024-04-12 12:42:40 +0545

    classwork

--- week1/tasks/Hello.java
+++ week1/tasks/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
         /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println(x:"Hello Worls")
     }
+    /*Extend/Modify the above program to print two additional lines:
+    This is my first program
+    I am on module ST4003CEM
+    System.out.print (not println) and produces the same output. */
+    System.out.print("This is my first program");
+    System.out.print("I am on modulw ST4003CEM");
     
+
+
+
+
 }
