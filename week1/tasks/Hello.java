f9aae52645ce81be8a74a070ea5912a9ed75896a
shreya <shreyakhadka007@gmail.com>
2024-04-26 11:44:12 +0545

    Week 1 wip

--- week1/tasks/Hello.java
+++ week1/tasks/Hello.java
@@ -1,19 +1,55 @@
-package week1;
 
 public class Hello {
     public static void main(String[] args) {
-        /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
-        System.out.println(x:"Hello Worls")
+        /*
+         * Write a program that prints Hello World! to the console. Save the program as
+         * Hello.java.
+         */
+        System.out.println("Hello World");
+
+        /*
+         * Extend/Modify the above program to print two additional lines:
+         * This is my first program
+         * I am on module ST4003CEM
+         * System.out.print (not println) and produces the same output.
+         */
+        System.out.print("This is my first program");
+        System.out.print("I am on module ST4003CEM");
+
+        /*
+         * Write a program that prints the following output:
+         * A "quoted" String is'much' better if you learn the rules of
+         * "escape sequences."
+         * Also, "" represents an empty String. Don't forget: use \"instead of " !'' is
+         * not the same as "
+         */
+        System.out.print(" A \" quoted \" is 'much' better if you learn the rules of \"escape sequesnces\" ");
+        System.out.print(
+                " \"\" represents an empty String. Don't forget: use \" instead of \" ! '' is not the same as \"");
+
+        /*
+         * 4. WAP that prints the following pattern:
+         *
+         **
+         ***
+         */
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        /*
+         * 5. WAP prints the following pattern:
+         *********
+         *********
+         *********
+         ********* 
+         * 
+         * 
+         * 
+         * /*
+         */
+
     }
-    /*Extend/Modify the above program to print two additional lines:
-    This is my first program
-    I am on module ST4003CEM
-    System.out.print (not println) and produces the same output. */
-    System.out.print("This is my first program");
-    System.out.print("I am on modulw ST4003CEM");
-    
-
-
-
 
 }
